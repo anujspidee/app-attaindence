6572704c0a770697ef7bde243ee6824545ff0fd5
anujspidee <drsantoshku@gmail.com>
2026-02-27 21:23:31 +0530

    main button create

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,17 @@
+"use client"
+import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
 import Image from "next/image";
 
 
 export default function Home() {
-  useTheme('light');
+ //useTheme('light');
 
   return (
     <div className="flex justify-center my-4" >
-      hello world  test2
+      main
+        <Button onClick={() => window.location.href = "/dashboard"}>Click me</Button>
+        
     </div>
   );
 }
